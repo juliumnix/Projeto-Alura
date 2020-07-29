e78099be9b916face0bde562b6c98dcffcee2846
juliumnix <juliumnix@users.noreply.github.com>
2020-07-28 23:20:32 -0300

    Cadastro categoria iniciado

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import PageDefault from "../../../components/PageDefault";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
-
-function CadastroCategoria () {
+function CadastroCategoria() {
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
-    
-      <Link to ="/">
-        Ir para a Home
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
       </Link>
     </PageDefault>
   )
-};
+}
 
 export default CadastroCategoria;
